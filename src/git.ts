bd72c28531039103dfa081e2b8fe361a0b4cceb0
Yi Feng Yang <yifeng@yfyang.dev>
2021-01-16 18:47:48 -0500

    Fix path

--- src/git.ts
+++ src/git.ts
@@ -1,13 +1,15 @@
 import simpleGit from 'simple-git';
 import type { SimpleGitOptions, SimpleGit, PullResult } from 'simple-git';
+import * as vscode from 'vscode';
 
-// const options: SimpleGitOptions = {
-//   baseDir: process.cwd(),
-//   binary: 'git',
-//   maxConcurrentProcesses: 10,
-// };
+const options: SimpleGitOptions = {
+  // screw deprecation amirite????
+  baseDir: vscode.workspace.rootPath ?? "",
+  binary: 'git',
+  maxConcurrentProcesses: 10,
+};
 
-export const gitInstance: SimpleGit = simpleGit();
+export const gitInstance: SimpleGit = simpleGit(options);
 
 export async function gitPull(): Promise<PullResult> {
   return await gitInstance.pull();
